54586cd445bcc4e2dd3c9587c661500fe170e7cc
thiagarto <thiagopirez01@gmail.com>
2022-06-19 16:46:24 -0300

    Envio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -241,7 +241,7 @@ function esVocal(letra){
   if(letra.length > 1){
     return "Dato incorrecto"
   }
-  else if(letra==="a"|| letra==="e"|| letra==="i"|| letra==="o"|| letra==="u")
+  else if(letra==="a"|| letra==="e"|| letra==="i"|| letra==="o"|| letra==="u" )
     {return "Es vocal"} else {
     return "Dato incorrecto"
   }
